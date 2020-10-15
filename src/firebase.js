c86801ac70259d06d8c5353cdff522a755bf3106
horsemaker <yash_ghodekar@yahoo.in>
2020-10-15 20:58:52 +0530

    Built Header.

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCGuzhbS8BpOLupnKWk2rNEFKr8YRnW7Og",
+    authDomain: "clone-11397.firebaseapp.com",
+    databaseURL: "https://clone-11397.firebaseio.com",
+    projectId: "clone-11397",
+    storageBucket: "clone-11397.appspot.com",
+    messagingSenderId: "650927543695",
+    appId: "1:650927543695:web:52c07deecb2ae82e53e7fa",
+    measurementId: "G-NLB8NESYDC"
+  };
